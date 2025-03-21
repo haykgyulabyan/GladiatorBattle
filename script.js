fce4a71a60e34a0a20afa20915a1d2d6b4741159
haykgyulabyan <gyulabyanh@gmail.com>
2025-03-21 19:17:45 +0400

    Added listener to create gladiators with all the stats on start battle button click

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import { faker } from 'https://cdn.skypack.dev/@faker-js/faker';
+
+const GLADIATORS_COUNT = 5;
 let gladiators = [];
 
 function calculateSpeed(gladiator) {
@@ -30,3 +33,33 @@ function log(message) {
   logDiv.appendChild(p);
   logDiv.scrollTop = logDiv.scrollHeight;
 }
+
+document.getElementById('start').addEventListener('click', () => {
+  document.getElementById('log').innerHTML = '';
+  document.getElementById('gladiators').innerHTML = '';
+  gladiators = [];
+
+  for (let i = 0; i < GLADIATORS_COUNT; i++) {
+    let name = faker.person.firstName();
+    let health = Math.floor(Math.random() * 21) + 80;
+    let power = (Math.floor(Math.random() * 31) + 20) / 10;
+    let speed = Math.random() * 4 + 1;
+    gladiators.push({
+                      name,
+                      health,
+                      initial_health: health,
+                      power,
+                      speed,
+                      initial_speed: speed,
+                      timeSinceLastAttack: 0
+                    });
+  }
+
+  gladiators.forEach(gladiator => {
+    let div = document.createElement('div');
+    div.className = 'gladiator';
+    div.id = `gladiator-${gladiator.name}`;
+    div.innerHTML = `<h3>${gladiator.name}</h3><p>Health: <span class='health'>${gladiator.health.toFixed(1)}</span></p><p>Power: ${gladiator.power.toFixed(1)}</p><p>Speed: ${gladiator.speed.toFixed(3)}</p>`;
+    document.getElementById('gladiators').appendChild(div);
+  });
+});
